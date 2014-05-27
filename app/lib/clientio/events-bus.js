f1bba18767cb1c74a615b5954968bbe477086497
Wiktor Obrębski <wiktor.obrebski@10clouds.com>
2014-05-27 11:28:04 +0200

    resolve #3 , system event bus

---
+++ app/lib/clientio/events-bus.js
@@ -0,0 +1,54 @@
+"use strict";
+
+namespace('clientio', function(undefined) {
+
+    /*
+     * how to use system event bus?
+     *
+     * //creating object
+     * function MyObject(){};
+     * //inherit from EventsBus
+     * MyObject.prototype = new clientio.EventsBus();
+     *
+     * var test = new MyObject();
+     * test.on('test', function() { alert(5); } );
+     * test.emit('test'); //alert(5)!
+     */
+
+    var __slice = Array.prototype.slice;
+
+    function EventsBus(){
+        this.listeners = {};
+    }
+
+    EventsBus.prototype.on = function(eventName, callback) {
+        if (this.listeners[eventName] === undefined) {
+          this.listeners[eventName] = [];
+        }
+        return this.listeners[eventName].push(callback);
+    };
+
+    EventsBus.prototype.emit = function(eventName) {
+        var args = [];
+        if(arguments.length > 1) {
+            args = __slice(arguments, 1);
+        }
+
+        var __listeners = this.listeners[eventName];
+        if(__listeners !== undefined) {
+            for(var i = 0; i < __listeners.length; i++) {
+                __listeners[i].apply(this, args);
+            }
+        }
+
+        // call asterix, if any registered
+        __listeners = this.listeners['*'];
+        if(__listeners !== undefined) {
+            for(var i = 0; i < __listeners.length; i++) {
+                __listeners[i].apply(this, [eventName].concat(args));
+            }
+        }
+    };
+
+    this.EventsBus = EventsBus;
+});
