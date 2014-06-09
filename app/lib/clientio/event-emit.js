ed026a4e9bfb8381b879055423503ea40cd8580a
fakk6 <fakk6@o2.pl>
2014-06-09 20:44:57 +0200

    js file for event emit, emit do not work properly yet

---
+++ app/lib/clientio/event-emit.js
@@ -0,0 +1,28 @@
+namespace('clientio', function() {
+  var ADDRESS = 'http://localhost:8080';
+  this.connect(ADDRESS, function (socket) {
+  
+    $('.event-emit-container').load('partial/event-emit.html', function () {
+    	$('#btn-event-emit').click(function () {
+    		var	eventName = $('#event-name').val();
+    		var	eventBody = JSON.parse($('#event-body').val());
+
+    		if ($.trim(eventName) !== '') {
+          socket.emit('passEvent', eventName, eventBody);
+    		} else {
+    			alert('Specify event name');
+    		}
+    	}).keypress(function (key) {
+    		if (key.which == 13) {
+    			$('#btn-event-emit').click();
+    		}
+    	});
+
+    	$('#event-name').keypress(function (key) {
+    		if (key.which == 13) {
+    			$('#btn-event-emit').focus().click();
+    		}
+    	});
+    });
+  });
+});
