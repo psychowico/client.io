c52530f1f53f63dfc84ae04ccf7b0eefd5c6a530
fakk6 <fakk6@o2.pl>
2014-06-13 20:45:44 +0200

    moved pEventBody out of try scope

---
+++ app/lib/clientio/event-emit.js
@@ -0,0 +1,34 @@
+namespace('clientio', function() {
+
+  this.eventEmit = function (socket) {
+    $('.btn-event-emit').click(function () {
+        var	eventName = $('.event-name').val();
+        var	eventBody = $('.event-body').val();
+        var pEventBody = null;
+
+        if ($.trim(eventName) !== '') {
+            try {
+                pEventBody = JSON.parse(eventBody);
+            } catch (err) {
+                alert('Unrecognized format of event body!');    
+            }
+
+            if (pEventBody != null) {
+                socket.emit('passEvent', eventName, pEventBody);
+            }
+        } else {
+           alert('Specify event name!');
+        }
+    }).keypress(function (key) {
+        if (key.which == 13) {
+            $('.btn-event-emit').click();
+        }
+    });
+
+    $('.event-name').keypress(function (key) {
+        if (key.which == 13) {
+            $('.btn-event-emit').focus().click();
+        }
+    });
+  };
+});
