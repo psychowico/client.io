dbfdb46244bc99df9ea07ee8a5ac32e8b52a5d78
fakk6 <fakk6@o2.pl>
2014-06-13 23:14:49 +0200

    changed vison on multiple emits

--- app/lib/clientio/event-emit.js
+++ app/lib/clientio/event-emit.js
@@ -1,40 +1,35 @@
 namespace('clientio', function() {
 
   this.eventEmit = function (socket, element) {
-    $('.btn-event-emit').click(function () {
-        var events = element.find('.event-wraper');
-        var eventNumber = 1; //in future can be replaced with event name provided in layout, or sth simillar
+    var $eventName = element.find('.event-name');
+    var $eventBody = element.find('.event-body');
+    var $eventButton = element.find('.btn-event-emit');
+    var pEventBody = null;
 
-        $.each(events, function () {
-           var eventName = $(this).find('.event-name').val()
-           var eventBody = $(this).find('.event-body').val()
-           var pEventBody = null;
-
-            if ($.trim(eventName) !== '') {
-                try {
-                    pEventBody = JSON.parse(eventBody);
-                } catch (err) {
-                    alert('Unrecognized format of '+ eventNumber +' event body!');    
-                }
-
-                if (pEventBody != null) {
-                    socket.emit('passEvent', eventName, pEventBody);
-                }
-            } else {
-               alert('Specify '+ eventNumber +' event name!');
+    $eventButton.click(function () {
+        if ($.trim($eventName.val()) !== '') {
+            try {
+                pEventBody = JSON.parse($eventBody.val());
+            } catch (err) {
+                alert('Unrecognized format of event body!');    
             }
-            eventNumber++;
-        });
+
+            if (pEventBody != null) {
+                socket.emit('passEvent', $eventName.val(), pEventBody);
+            }
+        } else {
+            alert('Specify event name!');
+        }
 
     }).keypress(function (key) {
         if (key.which == 13) {
-            $('.btn-event-emit').click();
+            $eventButton.click();
         }
     });
 
-    $('.event-name').keypress(function (key) {
+    $eventName.keypress(function (key) {
         if (key.which == 13) {
-            $('.btn-event-emit').focus().click();
+            $eventButton.focus().click();
         }
     });
   };
