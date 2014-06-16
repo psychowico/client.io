66bd1a3d62de315d3a01525973abd5b60ed7221d
Wiktor Obrębski <wiktor.obrebski@10clouds.com>
2014-06-16 11:49:27 +0200

    standarize code style

--- app/lib/clientio/connect.js
+++ app/lib/clientio/connect.js
@@ -2,46 +2,47 @@
 
 namespace('clientio', function () {
 
-  this.connect = function(address, success) {
-    $.getScript(address + '/socket.io/socket.io.js')
-     .done(libFinded).fail(libError);
+    this.connect = function (address, success) {
+        $.getScript(address + '/socket.io/socket.io.js')
+            .done(libFinded).fail(libError);
 
-    function libFinded() {
-      addWildcardSupport();
-      var socket = io.connect(address);
-      success(socket);
-    }
-    function libError(jqxhr, settings, exception ) {
-      console.error(exception);
-    }
-  };
+        function libFinded() {
+            addWildcardSupport();
+            var socket = io.connect(address);
+            success(socket);
+        }
 
-  function addWildcardSupport() {
-    // versions 0.8.*-0.9.*
-    if (io.SocketNamespace) {
-      io.SocketNamespace.prototype.$emit = (function ($orgEmit) {
-        return function () {
-          var args = Array.prototype.slice.call(arguments);
-          $orgEmit.apply(this, ['*'].concat(args));
-          $orgEmit.apply(this, arguments);
-        };
-      })(io.SocketNamespace.prototype.$emit);
-    }
-    //version 1.0.*+
-    else if (io.Socket.prototype.onevent) {
-      //more complicated here, finded by long research
-      io.Socket.prototype.onevent = (function (emit) {
-        return function (packet) {
-          var args = packet.data || []
+        function libError(jqxhr, settings, exception) {
+            console.error(exception);
+        }
+    };
 
-          if (packet.id) {
-            args.push(this.ack(packet.id));
-          }
+    function addWildcardSupport() {
+        // versions 0.8.*-0.9.*
+        if (io.SocketNamespace) {
+            io.SocketNamespace.prototype.$emit = (function ($orgEmit) {
+                return function () {
+                    var args = Array.prototype.slice.call(arguments);
+                    $orgEmit.apply(this, ['*'].concat(args));
+                    $orgEmit.apply(this, arguments);
+                };
+            })(io.SocketNamespace.prototype.$emit);
+        }
+        //version 1.0.*+
+        else if (io.Socket.prototype.onevent) {
+            //more complicated here, finded by long research
+            io.Socket.prototype.onevent = (function (emit) {
+                return function (packet) {
+                    var args = packet.data || []
 
-          this.io.emit.apply(this, args);
-          this.io.emit.apply(this, ['*'].concat(args));
-        };
-      })(io.Socket.prototype.emit);
+                    if (packet.id) {
+                        args.push(this.ack(packet.id));
+                    }
+
+                    this.io.emit.apply(this, args);
+                    this.io.emit.apply(this, ['*'].concat(args));
+                };
+            })(io.Socket.prototype.emit);
+        }
     }
-  }
 });
